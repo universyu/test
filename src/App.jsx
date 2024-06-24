3d8a47b79f1bcd4d493c3b1eb8f0c37152dad96d
Your Name <you@example.com>
2024-06-24 16:40:08 +0800

    34

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <>
     <h1>Hello</h1>
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/image" element={<img src="/1.jpg" alt="myImg" />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
     </>
     
   )
